50e7cc225a12b3e5fba4636f117b549802c78fa6
gilzoide <gilzoide@gmail.com>
2023-10-28 00:18:50 -0300

    Add documentation comments

--- Runtime/BackButtonStack.cs
+++ Runtime/BackButtonStack.cs
@@ -6,9 +6,13 @@ namespace Gilzoide.BackButtonStack
 {
     public class BackButtonStack : MonoBehaviour
     {
+        #region Singleton
+
+        /// <summary>Get or create the singleton instance.</summary>
         public static BackButtonStack Instance => _instance != null ? _instance : (_instance = CreateInstance());
         private static BackButtonStack _instance;
 
+        /// <returns>BackButtonStack instance with its GameObject marked with <see cref="DontDestroyOnLoad"/></returns>
         private static BackButtonStack CreateInstance()
         {
             var gameObject = new GameObject(nameof(BackButtonStack));
@@ -16,8 +20,11 @@ namespace Gilzoide.BackButtonStack
             return gameObject.AddComponent<BackButtonStack>();
         }
 
+        #endregion
+
+        /// <summary>Read-only view of the ESC/Back button handlers registered in the stack.</summary>
         public IReadOnlyList<IBackButtonHandler> BackButtonHandlers => _backButtonHandlers;
-        private readonly List<IBackButtonHandler> _backButtonHandlers = new();
+        private readonly List<IBackButtonHandler> _backButtonHandlers = new List<IBackButtonHandler>();
 
         private void Update()
         {
@@ -33,6 +40,13 @@ namespace Gilzoide.BackButtonStack
             }
         }
 
+        /// <summary>
+        /// Add the given handler to the top of the stack.
+        /// Adding an object that was already registered is not supported.
+        /// </summary>
+        /// <param name="handler">ESC/Back button handler</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="handler"/> is <see langword="null"/>.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when <paramref name="handler"/> is already present in the stack.</exception>
         public void AddButtonHandler(IBackButtonHandler handler)
         {
             if (handler == null)
@@ -47,6 +61,13 @@ namespace Gilzoide.BackButtonStack
             enabled = true;
         }
 
+        /// <summary>
+        /// Remove the given handler from anywhere in the stack.
+        /// </summary>
+        /// <remarks>
+        /// Passing an object that was not registered is a no-op.
+        /// </remarks>
+        /// <param name="handler">ESC/Back button handler</param>
         public void RemoveButtonHandler(IBackButtonHandler handler)
         {
             _backButtonHandlers.Remove(handler);
