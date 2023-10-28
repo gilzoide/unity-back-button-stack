03d644a84d37c734491538cf518c7767e7f7dc28
gilzoide <gilzoide@gmail.com>
2023-10-27 23:41:43 -0300

    Fix indentation level

--- Editor/BackButtonStackEditor.cs
+++ Editor/BackButtonStackEditor.cs
@@ -3,43 +3,43 @@ using UnityEngine;
 
 namespace Gilzoide.BackButtonStack.Editor
 {
-        [CustomEditor(typeof(BackButtonStack))]
-        class BackButtonStackEditor : UnityEditor.Editor
+    [CustomEditor(typeof(BackButtonStack))]
+    class BackButtonStackEditor : UnityEditor.Editor
+    {
+        [SerializeField] private bool foldout = true;
+
+        public override bool RequiresConstantRepaint()
         {
-            [SerializeField] private bool foldout = true;
+            return Application.isPlaying;
+        }
 
-            public override bool RequiresConstantRepaint()
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            BackButtonStack backButtonStack = (BackButtonStack) target;
+            if (Application.isPlaying && backButtonStack.enabled)
             {
-                return Application.isPlaying;
-            }
-
-            public override void OnInspectorGUI()
-            {
-                DrawDefaultInspector();
-
-                BackButtonStack backButtonStack = (BackButtonStack) target;
-                if (Application.isPlaying && backButtonStack.enabled)
+                foldout = EditorGUILayout.Foldout(foldout, "Back Button Handlers");
+                if (foldout)
                 {
-                    foldout = EditorGUILayout.Foldout(foldout, "Back Button Handlers");
-                    if (foldout)
+                    EditorGUI.indentLevel++;
+                    using (new EditorGUI.DisabledScope(true))
+                    for (int i = backButtonStack.BackButtonHandlers.Count - 1; i >= 0; i--)
                     {
-                        EditorGUI.indentLevel++;
-                        using (new EditorGUI.DisabledScope(true))
-                        for (int i = backButtonStack.BackButtonHandlers.Count - 1; i >= 0; i--)
+                        IBackButtonHandler backButtonHandler = backButtonStack.BackButtonHandlers[i];
+                        if (backButtonHandler is Object obj)
                         {
-                            IBackButtonHandler backButtonHandler = backButtonStack.BackButtonHandlers[i];
-                            if (backButtonHandler is Object obj)
-                            {
-                                EditorGUILayout.ObjectField($"{i}", obj, typeof(Object), true);
-                            }
-                            else
-                            {
-                                EditorGUILayout.TextField($"{i}", backButtonHandler.ToString());
-                            }
+                            EditorGUILayout.ObjectField($"{i}", obj, typeof(Object), true);
                         }
-                        EditorGUI.indentLevel--;
+                        else
+                        {
+                            EditorGUILayout.TextField($"{i}", backButtonHandler.ToString());
+                        }
                     }
+                    EditorGUI.indentLevel--;
                 }
             }
         }
+    }
 }
